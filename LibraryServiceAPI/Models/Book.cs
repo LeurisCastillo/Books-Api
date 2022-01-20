82fabae8ac04f224f4b021e5256aa8e54b8e8af4
LeurisCastillo <leurisa.delossantos@gmail.com>
2022-01-20 17:06:38 -0400

    Book model created

---
+++ LibraryServiceAPI/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryServiceAPI.Models
+{
+    public class Book
+    {
+
+        public class Rootobject
+        {
+            public int id { get; set; }
+            public string title { get; set; }
+            public string description { get; set; }
+            public int pageCount { get; set; }
+        }
+
+    }
+}
