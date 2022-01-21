69c513d34583f065910ad8514cf4a34005a40a3f
LeurisCastillo <leurisa.delossantos@gmail.com>
2022-01-21 00:21:05 -0400

    added httpClient as a dependency injection

--- consume-library-api/src/App.js
+++ consume-library-api/src/App.js
@@ -1,7 +1,38 @@
-import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-	return <div className="App"></div>;
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			items: [],
+			isLoaded: false,
+		};
+	}
+
+	componentDidMount() {
+		fetch("https://localhost:44331/api/Books")
+			.then((res) => res.json())
+			.then((json) => {
+				this.setState({
+					isLoaded: true,
+					items: json,
+				});
+			});
+	}
+
+	render() {
+		var { isLoaded, items } = this.state;
+
+		if (!isLoaded) {
+			return <div>Loading....</div>;
+		} else {
+			return (
+				<div className="App">
+					<h1>Data has been loaded</h1>
+				</div>
+			);
+		}
+	}
 }
 
 export default App;
